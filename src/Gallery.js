1366985bbc1c3ff10a995d7a350950477f3edacf
bruno <pascoal.goncalves.bruno@gmail.com>
2024-10-25 16:52:46 +0200

    début du site

---
+++ src/Gallery.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import Img1 from './img/1.jpeg'
+import Img2 from './img/2.jpeg'
+import Img3 from './img/3.jpeg'
+import Img4 from './img/4.jpeg'
+
+const Gallery = () => {
+
+    let data = [
+        {
+            id: 1,
+            imgSrc: Img1,
+        },
+        {
+            id: 2,
+            imgSrc: Img2,
+        },
+        {
+            id: 3,
+            imgSrc: Img3,
+        },
+        {
+            id: 4,
+            imgSrc: Img4,
+        },
+    ]
+
+  return (
+   <>
+   <div className='gallery'>
+     {data.map((item) => (
+       <div key={item.id} className='pics'>
+         <img src={item.imgSrc} alt='' style={{width: "100%"}} />
+       </div>
+     ))}
+   </div>
+   </>
+  )
+}
+
+export default Gallery
